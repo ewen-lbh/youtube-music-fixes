2001f6b7299a4e5f8ec5f8c890dee359e70065b8
Mx3 <ewen.lebihan7@gmail.com>
2019-03-10 15:38:09 +0100

    allow scheme usage and remove "(Original Mix)" from track name

--- tab-title.user.js
+++ tab-title.user.js
@@ -2,7 +2,7 @@
 // @name         YTM Tab Title Fix
 // @namespace    https://github.com/ewen-lbh/youtube-music-fixes/
 // @version      0.1
-// @description  Change tab title to "<artist> - <title>"
+// @description  Change tab title to according to a scheme, and remove "(Original Mix)" from title name
 // @author       Mx3
 // @match        *://music.youtube.com/*
 // @grant        none
@@ -12,10 +12,11 @@
     'use strict';
 
     let ch_title = function() {
-        let title_sep=" - "
+        let scheme = "<title> by <artist>"
         let trackname = document.querySelector('#layout > ytmusic-player-bar > div.middle-controls.style-scope.ytmusic-player-bar > div.content-info-wrapper.style-scope.ytmusic-player-bar > yt-formatted-string').innerText
+        trackname = trackname.replace('(Original Mix)', '')
         let artistname = document.querySelector('#layout > ytmusic-player-bar > div.middle-controls.style-scope.ytmusic-player-bar > div.content-info-wrapper.style-scope.ytmusic-player-bar > span > span.subtitle.style-scope.ytmusic-player-bar > yt-formatted-string > a:nth-child(1)').innerText
-        let newt = artistname+title_sep+trackname
+        let newt = scheme.replace('<artist>', artistname).replace('<title>', trackname)
         console.log('changing title to "'+newt+'"');
         document.title = newt
     }
